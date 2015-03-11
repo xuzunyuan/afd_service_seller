665b411166025a780c9e31ab570f986aa059fc1d
xuzunyuan <xuzunyuan@yiwang.com>
2015-03-11 11:23:42 +0800

    v

---
+++ src/main/java/com/afd/seller/dao/SellerReceiptMapper.java
@@ -0,0 +1,17 @@
+package com.afd.seller.dao;
+
+import com.afd.model.seller.SellerReceipt;
+
+public interface SellerReceiptMapper {
+    int deleteByPrimaryKey(Integer receiptId);
+
+    int insert(SellerReceipt record);
+
+    int insertSelective(SellerReceipt record);
+
+    SellerReceipt selectByPrimaryKey(Integer receiptId);
+
+    int updateByPrimaryKeySelective(SellerReceipt record);
+
+    int updateByPrimaryKey(SellerReceipt record);
+}
